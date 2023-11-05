b9f8cd751443ba0581912af649de75f5267369f1
Jimmy Hoke <jimmy@jimmyhoke.net>
2023-11-05 06:55:13 +0000

    WIP form. Nearly done.
    Co-authored-by: Keval Shah <kevalshah2005@users.noreply.github.com>

--- api/api.py
+++ api/api.py
@@ -2,9 +2,21 @@ from typing import Union
 import uvicorn
 import csv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/stats")
 def get_posts():
